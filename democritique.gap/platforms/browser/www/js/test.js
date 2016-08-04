4c1e59d440b9e64dfb543c19a38dc92cc84b575d
fruktstav <fruktstav@gmail.com>
2016-08-04 18:34:00 +0200

    Various backend fixes

---
+++ democritique.gap/platforms/browser/www/js/test.js
@@ -0,0 +1,69 @@
+document.addEventListener("deviceready", function(){
+
+    // Show spinner 
+    $('.load-bar').show();
+    
+	// User login
+
+                var userID = window.localStorage.getItem("userID"); // Get local cookie with stored User ID)
+
+
+                if(! userID){ // If the cookie doesn't exist
+
+
+                    var uuid = guid(); // Generates uniquish ID
+
+                    window.localStorage.setItem("userID", uuid); // Create a new cookie with the genereted ID
+
+                    addUser(uuid); // Ajax to upload data to database
+
+                };
+
+                // else{
+
+                //     getUserData(userID); // If user already exists, consider it logged in.
+                
+                // };
+
+    // Fetch first round of data
+
+    var pageCount = 0; //increment count for ajax download of entry posts
+
+                //first download of posts when page is loaded
+
+                $.post('http://lingon.funkar.nu/sites/riksdagen/php/fetchReports.php',{'page': pageCount}, function(data) { //ajax command
+               
+                    $("#results").append(data); //append data received from server
+                    clickHandlers();
+                    colorizeResults();
+
+                });
+   
+                pageCount++; //increment on page
+                $('.load-bar').hide();
+
+
+
+    if( $("#results").height() <  $(window).height() ) {
+
+        $('.load-bar').show();
+
+        $.post('http://lingon.funkar.nu/sites/riksdagen/php/fetchReports.php',{'page': pageCount}, function(data) {
+               
+            $("#results").append(data); //append data received from server
+            clickHandlers();
+            colorizeResults();
+   
+        pageCount++; //increment on page
+        $('.load-bar').hide();
+
+
+        })
+
+	}
+
+
+
+});
+
+	
